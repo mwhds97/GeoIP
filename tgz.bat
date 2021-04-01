5772c9134765bd907e0fc8aa4a0999a517370ae9
mwhds97 <cjctcj68@gmail.com>
2021-04-01 09:56:19 +0800

    Delete batch files

--- tgz.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-md GeoLite2-Country_20201231
-copy Country.mmdb GeoLite2-Country_20201231\GeoLite2-Country.mmdb
-7za a -ttar -so -an GeoLite2-Country_20201231 | 7za a -si GeoLite2-Country_20201231.tar.gz
-rd /s /q GeoLite2-Country_20201231
